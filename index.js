37bbc3e4feec6754a77d6adeb500cbbb08c519d3
Japheth Oruko <japhethoruko@gmail.com>
2024-10-01 11:57:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+// Initial cats array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive Function
+function destructivelyAppendCat(cat) {
+    cats.push(cat);
+}
+
+function destructivelyPrependCat(cat) {
+    cats.unshift(cat);
+}
+
+function  destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+// Non-destructive functions
+function appendCat(name){
+    return[...cats, name]
+}
+
+function prependCat(name){
+    return [name, ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0,cats.length -1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
